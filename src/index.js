260e8b8c867778aad41c78bd93c8bc872bbeba7c
Irakli Gogicha <108423675+gogicha007@users.noreply.github.com>
2023-08-08 21:03:53 +0400

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let strArray = str.split('')
+  let i = strArray.length
+  let closed = false
+
+  const objConfig = bracketsConfig.reduce((object, value) => {
+      return { ...object, [value[0]]: value[1] }
+  }, {})
+
+  while (i--) {
+      if (i > 0) {
+          // console.log(`${strArray[i - 1]} : ${strArray[i]} : ${strArray[i] === objConfig[strArray[i - 1]]}`)
+          if (strArray[i] === objConfig[strArray[i - 1]]) {
+              strArray.splice(i - 1, 2, '')
+              strArray = strArray.filter((item) => item)
+              i = strArray.length
+              closed = true
+          } else {
+              closed = false
+          }
+      } else { closed = false }
+  }
+  return closed
 }
